490df73d2c5d906b7406bff5f7ef320f893b5509
= <=>
2016-09-22 20:00:57 +0800

    second time

---
+++ UI.js
@@ -0,0 +1,16 @@
+
+var options = [
+  {
+    title: "启动完毕",
+    body: "主窗口启动"
+  },
+  {
+    title: "Content-Image Notification",
+    body: "Short message plus a custom content image",
+  }
+]
+
+function notify() {
+loadingWindow.close();
+  //new Notification(options[0].title, options[0]);
+}
